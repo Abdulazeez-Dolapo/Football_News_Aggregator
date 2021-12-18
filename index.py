37a2c64c9f3a927e78cf4f8257fda2a092ba7008
abdulazeez-dolapo <abdulrafiua10@gmail.com>
2021-12-18 20:38:32 +0100

    add the other F365 articles to the email content

--- index.py
+++ index.py
@@ -27,53 +27,50 @@ def extract_soup(url: str) -> BeautifulSoup:
     return soup
 
 
-def format_football365_soup() -> str:
+def format_football365_article_list(article_list: list, start_count: int = 1) -> str:
+    """
+    Format the F365 list of articles into a stringified list of links in the format
+    ``serial_number``:: ``article_type`` | ``headline``
+    """
+
+    content = ""
+    headlines = {}
+
+    for i, tag in enumerate(article_list):
+        link = tag.find("a")
+
+        text = link.get_text(" | ", strip=True)
+        text_list = text.split(" | ")
+        headline = text_list[1] if len(text_list) == 3 else text_list[0]
+        text_to_use = " | ".join(text_list[:-1])
+
+        href = link["href"]
+        newLink = f'<a href="{href}">{text_to_use}</a>'
+        count = i + start_count
+
+        # This is required to remove duplicate links
+        if headline in headlines:
+            continue
+        else:
+            headlines[headline] = count
+            content += f"{(str(count))} :: {newLink}\n<br/>"
+
+    return content
+
+
+def fetch_football365_articles() -> str:
     """returns the formatted html of the football365 site"""
 
     content = "<h2>FOOTBALL365</h2>"
 
     soup = extract_soup("https://football365.com")
-
-    for i, tag in enumerate(soup.find_all("li", attrs={"class": "hero__item"})):
-        link = tag.find("a")
-
-        text = link.get_text(" | ", strip=True)
-        text = text.split(" | ")
-        text = text[:-1]
-        text = " | ".join(text)
-
-        href = link["href"]
-        newLink = f'<a href="{href}">{text}</a>'
-
-        content += f"{(str(i + 1))} :: {newLink}\n<br/>"
+    articles = soup.find_all("li", ["hero__item", "articleList__item"])
+    content += format_football365_article_list(articles)
 
     return content
 
 
-#  print("Extracting news from " + url)
-# print("==========================")
-
-#  newContent = ""
-#  newContent += "<b>HN Top Stories:</b>\n<br>" + "-" * 50 + "<br>"
-
-
-# newContent = extract_soup("https://news.ycombinator.com")
-
-# content += newContent
-# content += "<br>----------<br>"
-# content += "<br><br>End of Message"
-
-# format_football365_soup()
-content = format_football365_soup()
-
-# print(content)
-
-# string = "eyes | on | my"
-# sl = string.split(" | ")
-# sl = sl[:-1]
-# sl = " | ".join(sl)
-# print(sl)
-
+content = fetch_football365_articles()
 recipients = ["abdulrafiua10@gmail.com"]
 subject = f"Football News {get_current_date()}"
 
